bfee970c7bc57ac812781e91e993478e00455e2f
Maxime Boue <abriko@yahoo.fr>
2017-09-10 13:18:10 +0200

    Add skills web design and coheris experience

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+
+$(document).ready(function(){
+  $('.btn-large2').hide();
+
+  $('.btn-large').on('click', function(){
+      $('.btn-large2').slideDown( "slow", function() {
+        // action at the end of animation
+    });
+  });
+});
